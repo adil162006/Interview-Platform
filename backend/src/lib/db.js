a209460ff09c2c9fd10710df0d71427b601517ef
Mohd Adil <shaikhmdadil191@gmail.com>
2025-10-31 20:39:35 +0530

    database and tools setups done

---
+++ backend/src/lib/db.js
@@ -0,0 +1,18 @@
+import mongoose from "mongoose"
+import { ENV } from "./env.js"
+
+export const connectDb=async ()=>{
+    try {
+        
+         if (!ENV.DB_URL) {
+      throw new Error("DB_URL is not defined in environment variables");
+    }
+        const conn = await mongoose.connect(ENV.DB_URL)
+        console.log("connected to mongodb",conn.connection.host);
+        
+    } catch (error) {
+        console.log("error connecting to database",error);
+        process.exit(1); //0 means success , 1 means failure
+        
+    }
+}
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,5 +1,6 @@
 import express from "express";
 import { ENV } from "./lib/env.js";
+import { connectDb } from "./lib/db.js";
 const app =express();
 
 
@@ -7,4 +8,18 @@ const port = ENV.PORT || 3000;
 app.get("/",(req,res)=>{
     res.send("Hello World");
 })
-app.listen(port,()=> console.log(`Server is running on port ${port}`));
+
+
+const startServer = async() =>{
+    try {
+        await connectDb();
+        app.listen(port,()=> {
+            console.log(`Server is running on port ${port}`)
+        });
+    } catch (error) {
+        console.log("error starting the server",error);
+        
+    }
+}
+
+startServer()
