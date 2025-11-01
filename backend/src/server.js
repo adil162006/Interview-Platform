ea5154db1e10c2e8c100018797034f1f156a278a
Mohd Adil <shaikhmdadil191@gmail.com>
2025-11-01 19:54:23 +0530

    added deployment config and syncing Auth with webhooks

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,13 +1,36 @@
 import express from "express";
 import { ENV } from "./lib/env.js";
 import { connectDb } from "./lib/db.js";
+import path from "path";
+import cors from "cors";
+import { inngest, functions } from "./lib/inngest.js";
+import { serve } from "inngest/express";
+import { clerkMiddleware } from "@clerk/express";
 const app =express();
 
+
+const __dirname = path.resolve();
+// middleware
+app.use(express.json());
+// credentials:true meaning?? => server allows a browser to include cookies on request
+app.use(cors({ origin: ENV.CLIENT_URL, credentials: true }));
+
+
+
+
+app.use("/api/inngest", serve({ client: inngest, functions }));
 
 const port = ENV.PORT || 3000;
 app.get("/",(req,res)=>{
     res.send("Hello World");
 })
+if (ENV.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/dist")));
+
+  app.get("/{*any}", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+  });
+}
 
 
 const startServer = async() =>{
